b3681d5ca3a04398e4546c0e9232ad4bede79373
Jeff Geerling <geerlingguy@mac.com>
2021-02-28 12:26:37 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Overlay Checklist / Task Tracker for OBS
+
+TODO.
+
+## Node.js App setup
+
+Install dependencies:
+
+```
+npm install
+```
+
+Start the server:
+
+```
+node server.js
+```
---
+++ server.js
@@ -0,0 +1,29 @@
+// Load the express module.
+var express = require('express');
+var app = express();
+var path = require('path');
+var step = 1;
+
+// Respond to requests for / with index.html.
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '', 'index.html'));
+});
+
+// Respond to requests for /current with current step.
+app.get('/current', function(req, res){
+    res.send(String(step));
+});
+
+// Allow incrementing or decrementing the step via /up or /down.
+app.get('/up', function(req, res){
+    step++;
+    res.send('New value: ' + step);
+});
+app.get('/down', function(req, res){
+    step--;
+    res.send('New value: ' + step);
+});
+
+// Listen on port 80 (like a true web server).
+app.listen(80);
+console.log('Express server started successfully.');
